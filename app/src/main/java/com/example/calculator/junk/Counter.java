13ca6977315d3e007175049a23653501beef2bed
Kondratyuk <kamiko1409@gmail.com>
2021-10-13 17:48:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/junk/Counter.java
@@ -0,0 +1,42 @@
+package com.example.calculator.junk;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Counter implements Parcelable {
+
+    private int value;
+
+    protected Counter(Parcel in) {
+        value = in.readInt();
+    }
+
+    public static final Creator<Counter> CREATOR = new Creator<Counter>() {
+        @Override
+        public Counter createFromParcel(Parcel in) {
+            return new Counter(in);
+        }
+
+        @Override
+        public Counter[] newArray(int size) {
+            return new Counter[size];
+        }
+    };
+
+    public int getValue() {
+        return value;
+    }
+
+    public void increase() {
+        value++;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+    }
+}
---
+++ app/src/main/java/com/example/calculator/junk/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.calculator.junk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.calculator.R;
+import com.example.calculator.ui.CalculatorActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
